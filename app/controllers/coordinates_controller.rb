2bf93661f2eb8a249f351fb975c101f33a9a3d19
keita.nakagawa <saku.nk@icloud.com>
2019-10-12 22:30:44 +0900

    Update coordinates_controller.rb

--- app/controllers/coordinates_controller.rb
+++ app/controllers/coordinates_controller.rb
@@ -1,4 +1,21 @@
 class CoordinatesController < ApplicationController
   def index
   end
+
+  def new
+    @coordinate = Coordinate.new
+  end
+
+  def create
+    @coordinate = Coordinate.new(coordinate_params)
+    @coordinate.save
+    redirect_to root_path
+  end
+
+  private
+
+  def coordinate_params
+    params.require(:coordinate).permit(:image, :item_detail).merge(user_id: current_user.id)
+  end
+  
 end
