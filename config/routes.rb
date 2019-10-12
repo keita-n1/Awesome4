f5fb29e8d284f60060c7a2a42b596cc0e4d669c4
keita.nakagawa <saku.nk@icloud.com>
2019-10-12 22:41:30 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :coordinates, only: [:index, :create]
-  resources :comments, only: [:index]
-  resources :users, only: [:show, :edit, :update] do
-    resources :coordinates, only: [:new]
+  resources :users, only: [:create, :show, :edit, :update] do
+    resources :coordinates, only: [:new] do
+      resources :comments, only: [:index, :create]
+    end
   end
   root "coordinates#index"
 end
