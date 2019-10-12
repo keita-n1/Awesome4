eb18ba77841b31ed009a4f78d8f9466a70f64a2e
keita.nakagawa <saku.nk@icloud.com>
2019-10-12 20:35:45 +0900

    add column users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_104914) do
+ActiveRecord::Schema.define(version: 2019_10_12_113322) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,11 @@ ActiveRecord::Schema.define(version: 2019_10_12_104914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "gender"
+    t.integer "age"
+    t.integer "height"
+    t.text "profile"
+    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
